a50d221a9e295c0065565e0e0ef3ba3432a302fd
Nico Schwanebeck <nico@nischwan.de>
2017-04-07 16:33:39 +0200

    fix localization

--- baronbale_website/baronbale_website/settings.py
+++ baronbale_website/baronbale_website/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 
-VERSION = '1.2'
-VERSION_NR = 2
+VERSION = '1.2.1'
+VERSION_NR = 3
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
