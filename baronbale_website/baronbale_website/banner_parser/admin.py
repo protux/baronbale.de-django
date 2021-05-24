e37ea533b1a146a738d6bbac09723f20db5928d3
Schwanebeck, Nico <Nico.Schwanebeck@adesso.de>
2021-05-24 09:11:38 +0200

    add new models to banner parser

--- baronbale_website/baronbale_website/banner_parser/admin.py
+++ baronbale_website/baronbale_website/banner_parser/admin.py
@@ -4,7 +4,23 @@ from baronbale_website.banner_parser import models
 
 
 class EnqueuedJobAdmin(admin.ModelAdmin):
-    list_display = ('ticket_id', 'email_address_to_notify', 'uploaded_file', 'time_created',)
+    list_display = (
+        "ticket_id",
+        "email_address_to_notify",
+        "uploaded_file",
+        "time_created",
+        "time_finished",
+    )
+
+
+class BannerDimensionAdmin(admin.ModelAdmin):
+    list_display = ("gc_code", "ratio", "url")
+
+
+class BannerCacheAdmin(admin.ModelAdmin):
+    list_display = ("gc_code", "reason", "image_url", "permanent")
 
 
 admin.site.register(models.EnqueuedBannerParserJob, EnqueuedJobAdmin)
+admin.site.register(models.BannerDimension, BannerDimensionAdmin)
+admin.site.register(models.BannerCache, BannerCacheAdmin)
--- baronbale_website/baronbale_website/banner_parser/models.py
+++ baronbale_website/baronbale_website/banner_parser/models.py
@@ -11,3 +11,21 @@ class EnqueuedBannerParserJob(models.Model):
     email_address_to_notify = models.CharField(null=True, blank=True, max_length=100)
     uploaded_file = models.FileField(verbose_name=_("GPX-File or Zip Archive"))
     time_created = models.DateTimeField(auto_now_add=True)
+    result = models.TextField(null=True, blank=True)
+    time_finished = models.DateTimeField(null=True, blank=True)
+
+
+class BannerDimension(models.Model):
+    gc_code = models.CharField(max_length=20)
+    banner = models.CharField(max_length=64)
+    url = models.CharField(max_length=1024)
+    ratio = models.FloatField(default=0.0)
+    width = models.IntegerField()
+    height = models.IntegerField()
+
+
+class BannerCache(models.Model):
+    gc_code = models.CharField(max_length=10)
+    image_url = models.CharField(max_length=255, null=True, blank=True)
+    reason = models.CharField(max_length=100)
+    permanent = models.BooleanField(default=False)
