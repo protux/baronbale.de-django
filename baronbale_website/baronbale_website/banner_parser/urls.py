6c7d0e7679f5db730872e83740dd5b0550353b27
Schwanebeck, Nico <Nico.Schwanebeck@adesso.de>
2021-05-24 15:18:51 +0200

    display banners when ready

--- baronbale_website/baronbale_website/banner_parser/urls.py
+++ baronbale_website/baronbale_website/banner_parser/urls.py
@@ -4,9 +4,11 @@ from baronbale_website.banner_parser.views.upload_listing_files_view import (
     UploadListingFilesView,
 )
 from baronbale_website.banner_parser.views.waiting_queue_view import WaitingQueueView
+from baronbale_website.banner_parser.views.banners_ready_view import BannersReadyView
 
 app_name = "banner_parser"
 urlpatterns = [
     path("", UploadListingFilesView.as_view(), name="upload_listing_files"),
     path("queue/<str:ticket_id>", WaitingQueueView.as_view(), name="queue"),
+    path("show/<str:ticket_id>", BannersReadyView.as_view(), name="show_banners"),
 ]
---
+++ baronbale_website/baronbale_website/banner_parser/views/banners_ready_view.py
@@ -0,0 +1,33 @@
+from django.views.generic import TemplateView
+from django.http.response import HttpResponseRedirect
+from django.urls import reverse
+
+from baronbale_website.banner_parser.models import BannerParserJob
+
+
+class BannersReadyView(TemplateView):
+    template_name = "banner_parser/banners_ready.html"
+
+    def get(self, request, *args, **kwargs):
+        banner_parser_job = self.fetch_banner_parser_job()
+        if banner_parser_job.result is None:
+            return HttpResponseRedirect(reverse('banner_parser:queue', args=[banner_parser_job.ticket_id]))
+
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+
+        banner_parser_job = self.fetch_banner_parser_job()
+
+        context_data['banner_parser_job'] = banner_parser_job
+        return context_data
+
+    def fetch_banner_parser_job(self):
+        banner_parser_job = BannerParserJob.objects.get(
+            ticket_id=self.get_ticket_id_from_path_param()
+        )
+        return banner_parser_job
+
+    def get_ticket_id_from_path_param(self) -> str:
+        return self.kwargs["ticket_id"]
