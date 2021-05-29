1d5717a89c171852d8264e94bd4ec9da388d6261
Schwanebeck, Nico <Nico.Schwanebeck@adesso.de>
2021-05-29 12:35:24 +0200

    update locales

--- baronbale_website/baronbale_website/banner_parser/forms/add_email_address_form.py
+++ baronbale_website/baronbale_website/banner_parser/forms/add_email_address_form.py
@@ -1,5 +1,6 @@
 from django import forms
+from django.utils.translation import gettext as _
 
 
 class AddEmailAddressForm(forms.Form):
-    email_address = forms.EmailField()
+    email_address = forms.EmailField(label=_('Email Address'))
